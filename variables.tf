600be218447d535497f65e8f71752adc1f3219b6
Mattias Fjellström <mattias.fjellstrom@gmail.com>
2023-11-27 17:19:36 +0100

    Add more validation on name suffix

--- variables.tf
+++ variables.tf
@@ -1,5 +1,7 @@
 variable "location" {
-  type = string
+  type        = string
+  default     = "swedencentral"
+  description = "Azure location for resource group"
 
   validation {
     condition = contains([
@@ -12,10 +14,16 @@ variable "location" {
 }
 
 variable "name_suffix" {
-  type = string
+  type        = string
+  description = "Resource group name suffix (i.e. rg-<NAME_SUFFIX>)"
 
   validation {
     condition     = !startswith(var.name_suffix, "rg-")
     error_message = "Do not add 'rg-' in the name, this is added automatically"
+  }
+
+  validation {
+    condition     = length("rg-${var.name_suffix}") <= 90
+    error_message = "Resource group name too long (should be between 1 and 90 characters)"
   }
 }
